945abf751f6dcb34cf5243210cff691c27fcd048
Kunal Tajne <kunal.tajne96@gmail.com>
2024-04-08 12:24:29 -0400

    app config

--- src/main/java/com/shopstream/config/AppConfig.java
+++ src/main/java/com/shopstream/config/AppConfig.java
@@ -1,6 +1,5 @@
 package com.shopstream.config;
 
-import com.shopstream.config.JwtTokenValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
